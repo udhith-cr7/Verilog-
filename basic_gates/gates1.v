21eb42f747dffe4ace8803ce04d6b63aa207c9f2
Udhith <judhithvlsi@gmail.com>
2025-05-27 20:43:15 +0530

    Update gates1.v

--- basic_gates/gates1.v
+++ basic_gates/gates1.v
@@ -1,7 +1,6 @@
 module basic_gates(
   input a,b,
   output out_and,out_or,out_nand,out_nor,out_xor,out_xnor,out_nota,out_notb);
-  
   assign out_and=a&b;
   assign out_or=a|b;
   assign out_nand=~(a&b);

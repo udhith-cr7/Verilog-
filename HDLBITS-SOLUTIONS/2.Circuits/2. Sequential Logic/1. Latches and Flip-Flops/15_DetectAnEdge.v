e2f6df9b93ba0a1670bef47ab206d0c2460b6c35
Udhith <judhithvlsi@gmail.com>
2025-06-14 22:27:02 +0530

    Update 15_DetectAnEdge.v

--- HDLBITS-SOLUTIONS/2.Circuits/2. Sequential Logic/1. Latches and Flip-Flops/15_DetectAnEdge.v
+++ HDLBITS-SOLUTIONS/2.Circuits/2. Sequential Logic/1. Latches and Flip-Flops/15_DetectAnEdge.v
@@ -3,18 +3,10 @@ module top_module (
     input [7:0] in,
     output [7:0] pedge
 );
-  integer i;
-    reg [7:0] in_tmp;
-    always @(posedge clk) begin
-        for(i = 0; i < 8; i = i + 1) begin
-            if(in_tmp[i] != in[i] && in[i] == 1) begin
-                pedge[i] = 1;
-            end
-            else begin
-                pedge[i] = 0;
-            end
-            in_tmp[i] = in[i];
-        end
-    end
+  reg [7:0] in_d;
 
+    always @(posedge clk) begin
+        pedge <= ~in_d & in;  
+        in_d <= in;           
+    end
 endmodule
